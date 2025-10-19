0c4092b154d58453e8f930ffd3e3f5a84c5a6aac
yewon-hana <=>
2025-10-19 22:30:32 +0900

    upload page first commit

--- frontend/src/pages/Mypage/layout/Upload.jsx
+++ frontend/src/pages/Mypage/layout/Upload.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const Upload =()=>{
+
+    const navigation = useNavigate();
+    const [quotes,setQuotes]=useState([]);
+    
+        useEffect(()=>{
+            /**사용자가 upload 한 quote 모두 불러오기 */
+            /**dummy */
+            setQuotes([{id:0,title:'little princess',content:'once upon a time~',creater:'b111',createdAt:'2000-10-22'},
+                        {id:32,title:'princess and the frog',content:'almot there',creater:'abab',createdAt:'2025-10-11'}
+            ])
+        },[]);
+    
+         const onDetail=(id)=>{
+            navigation('/detail/'+id);
+        }
+
     return(
         <>
-        upload
+        <div className="flex flex-col justify-items-center m-3">
+            {quotes.map((q)=>(
+        <div key={q.id} className=" cursor-pointer mt-5 border-2 hover:bg-main-beige border-main-green h-full pt-5 pb-5 text-center rounded-lg shadow-lg" 
+        onClick={() => onDetail(q.id)}> 
+            <div className=" pl-3 pb-3 text-sm text-start">{q.title} </div>
+            <div className="text-lg">{q.content}</div>
+            <div className="text-xs text-end pr-3"><span className="mr-2 font-bold">{q.creater}</span>{q.createdAt}</div>
+          </div>
+        ))}
+        </div>
         </>
     );
 }
--- frontend/src/pages/SearchList/index.jsx
+++ frontend/src/pages/SearchList/index.jsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from "react";
 import { Search } from "../../components/Search";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const SearchList =()=>{
 
+    const navigation = useNavigate();
     const {input} = useParams();
+    /** 검색어가 포함된 db 결과 조회 (quotes, book, drama, movie) */
+    const [result, setResult]=useState([]);
+
+    useEffect(()=>{
+        /**dummy */
+        setResult([{id:0,title:'little princess',content:'once upon a time~'},
+                    {id:32,title:'princess and the frog',content:'almot there'}
+        ])
+    },[input]);
+
+     const onDetail=(id)=>{
+        navigation('/detail/'+id);
+    }
 
     return(
         <>
         <div className="flex justify-center">
         <Search prevInput={input}/>
         </div>
-         <div className="flex mt-10 justify-center">
+         <div className="text-main-green flex mt-10 justify-center">
             " {input} " results
         </div>
-        </>
+        <div className="flex flex-col justify-items-center mt-7">
+            {result.map((r)=>(
+        <div key={r.id} className=" cursor-pointer mt-5 border-2 hover:bg-main-beige border-main-green h-full pt-5 pb-5 text-center rounded-lg shadow-lg" onClick={() => onDetail(r.id)}> 
+            <div className=" pl-3 pb-3 text-sm text-start">{r.title} </div>
+            <div className="text-lg">{r.content}</div>
+          </div>
+        ))}
+        </div>
+        </>        
     );
 }
