1be7b86949c21c01eba34d7c183da0da9383752c
wlsdnr710 <wlsdnr710@naver.com>
2025-10-31 12:33:16 +0900

    redundant api deleted

--- frontend/src/hooks/useUser.jsx
+++ frontend/src/hooks/useUser.jsx
@@ -1,37 +1,32 @@
 import { createContext, useContext } from "react";
-import api from '../util/api.js';
+import axios from "../api/axios";
 
 const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-
-  const getuser = async ({userid}) => {
-
+  const getuser = async ({ userid }) => {
     try {
-      const response = await api.get("/users", {
-       userid
+      const response = await axios.get("/api/users", {
+        userid,
       });
 
       if (response.status === 200) {
         return response.data;
       }
 
-      if(response.status === 404)
-        alert(response.data.detail);
+      if (response.status === 404) alert(response.data.detail);
       return false;
-
     } catch (error) {
-      setSevError(error.response?.data.detail || "회원정보 열람에 실패했습니다.");
+      // setSevError(error.response?.data.detail || "회원정보 열람에 실패했습니다.");
+      console.log(
+        error.response?.data.detail || "회원정보 열람에 실패했습니다."
+      );
       return false;
     }
   };
 
   return (
-    <UserContext.Provider
-      value={{getuser }}
-    >
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={{ getuser }}>{children}</UserContext.Provider>
   );
 };
 
